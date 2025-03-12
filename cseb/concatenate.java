0350c79c0f257cf53867aaec0e548e41841995b1
Sravana20 <sravanasanu555@gamil.com>
2025-03-12 15:03:43 +0530

    fix: concatenate

---
+++ cseb/concatenate.java
@@ -0,0 +1,12 @@
+public class concatenate{
+
+    public static void main(String[]args){
+        String a = "john";
+        String b =  "eats";
+        String c = "grapes";
+
+        System.out.println(a+b+c);
+        System.out.println(b.concat(c));
+
+    }
+}
