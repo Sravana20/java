d10402984b37af6d09b17d5f2c54cd362c032f0f
Sravana20 <sravanasanu555@gmail.com>
2025-03-13 14:46:50 +0530

    condition01

---
+++ cseb/conditions.java
@@ -0,0 +1,12 @@
+public class conditions{
+    public static void main(String[]args){
+        int age = 22;
+        System.out.println("Age = +age");
+        if(age>=18){
+            System.out.println("you are eligible to vote.");
+        }
+        else{
+            System.out.println("you aren't eligible to vote.");
+        }
+        }
+}
