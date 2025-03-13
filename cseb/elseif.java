6ec391764a7176dc84db6a3928cca366ef14c458
Sravana20 <sravanasanu555@gmail.com>
2025-03-13 15:36:47 +0530

    elseif

---
+++ cseb/elseif.java
@@ -0,0 +1,15 @@
+public class elseif{
+    public static void main(String[]args){
+
+        int m = 80;
+
+        if(m>=90){
+            System.out.println("Grade O");
+        }else if(m>=80){
+            System.out.println("Grade A");
+
+        }else if(m>=70){
+            System.out.println("Grade B");
+        }
+    }
+}
