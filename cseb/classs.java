e825378230fb7b2c71e6e320bf1411b0f038de32
Sravana20 <sravanasanu555@gmail.com>
2025-03-14 15:39:12 +0530

    classs

---
+++ cseb/classs.java
@@ -0,0 +1,19 @@
+public class classs
+{
+ static   class student
+    {
+        int id;
+        String name;
+    }
+    public static  class Teststudent
+    {
+        public static void main(String[]args)
+        {
+            student s1 = new student();
+            s1.id=101;
+            System.out.println(s1.id);
+        }
+
+        
+    }
+}
