c1380b2dd417581e3c39ce80651232424e349476
Sravana20 <sravanasanu555@gmail.com>
2025-03-14 10:41:00 +0530

    whiletwo

---
+++ cseb/whiletwo.java
@@ -0,0 +1,14 @@
+public class whiletwo
+{
+    public static void main(String[]args)
+    {
+        int i = 1;
+        while (i<=5)
+        {
+            System.out.println(10);
+            i++;
+        }
+    }
+}
+
+
