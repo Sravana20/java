0f4e49d06ede1594be75f05ccf8c1427798ffd25
Sravana20 <sravanasanu555@gmail.com>
2025-03-14 11:16:29 +0530

    stringornot

---
+++ cseb/stringornot.java
@@ -0,0 +1,17 @@
+public class stringornot
+{
+    public static void main(String[]args)
+    {
+        String s = "He like Apple";
+        
+        if(s.contains("like"))
+        {
+            System.out.println("Yes");
+    
+        }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+}
