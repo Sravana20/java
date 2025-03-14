3cbad03f0ed0b67d19a600be99ab2cc237317dde
Sravana20 <sravanasanu555@gmail.com>
2025-03-14 14:01:42 +0530

    lenarray

---
+++ cseb/lenarray.java
@@ -0,0 +1,8 @@
+public class lenarray
+{
+    public static void main(String[]args)
+    {
+        String[] a = {"Sivarebeesha","Samvritha","Aleena"};
+        System.out.println(a.length);
+    }
+}
