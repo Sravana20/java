776292f28ded3ae264318fd6845b5b7a6a0f43be
Sravana20 <sravanasanu555@gmail.com>
2025-03-14 14:42:00 +0530

    replaceindex

---
+++ cseb/replaceindex.java
@@ -0,0 +1,11 @@
+public class replaceindex
+{
+    public static void main(String[]args)
+    {
+        String[] fruits = {"Apple","Kiwi","Mango"};
+        fruits[0] = "Orange";
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+    }
+}
