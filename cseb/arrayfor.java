de09cb34077592e6abc872d90efafce0b92a6ac6
Sravana20 <sravanasanu555@gmail.com>
2025-03-14 15:29:53 +0530

    arrayfor

---
+++ cseb/arrayfor.java
@@ -0,0 +1,13 @@
+public class arrayfor
+{
+    public static void main(String[]args)
+    {
+        String[] fruits = {"Apple","Kiwi","Mango","Dragonfruit","Banana"};
+        for(int i = 0; i<=5;i++)
+
+        
+        {
+            System.out.println(fruits[i]);
+        }
+    }
+}
