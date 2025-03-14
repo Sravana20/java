e9e49b4919dd5fbd46ab6e10c328f94aecd2b47e
Sravana20 <sravanasanu555@gmail.com>
2025-03-14 13:54:14 +0530

    array

---
+++ cseb/array.java
@@ -0,0 +1,11 @@
+public class array
+{
+    public static void main(String[]args)
+    {
+        String fruits[] = {"Apple","Kiwi","Mango"};
+        
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+    }
+}
